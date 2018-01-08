647a1fbf16fac8e8a95cfe06e718187c5634c7e4
kokonattuDream <32606269+kokonattuDream@users.noreply.github.com>
2018-01-08 15:35:22 -0400

    Add files via upload

--- src/lettcodePractice/SelfDrivingNumber.java
+++ src/lettcodePractice/SelfDrivingNumber.java
@@ -3,6 +3,15 @@ package lettcodePractice;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A self-dividing number is a number that is divisible by every digit it contains.
+
+For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+
+Also, a self-dividing number is not allowed to contain the digit zero.
+ * 
+ *iven a lower and upper number bound, output a list of every possible self dividing number, including the bounds if possible.
+ */
 public class SelfDrivingNumber {
 	public List<Integer> selfDividingNumbers(int left, int right) {
         List<Integer> res = new ArrayList<Integer>();
