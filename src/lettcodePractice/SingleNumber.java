84ca500af1d924a770443032a9165916aa86e81e
kokonattuDream <32606269+kokonattuDream@users.noreply.github.com>
2018-01-10 12:38:01 -0400

    Add files via upload

---
+++ src/lettcodePractice/SingleNumber.java
@@ -0,0 +1,11 @@
+package lettcodePractice;
+
+public class SingleNumber {
+	public int singleNumber(int[] nums) {
+		int res = 0;
+        for(int i: nums){
+        	res ^= i;
+        }
+        return res;
+    }
+}
