ab2e69b96d20c1b04b7610da6e14da3b10b361cb
kokonattuDream <32606269+kokonattuDream@users.noreply.github.com>
2018-01-17 15:48:57 -0400

    Add files via upload

---
+++ src/lettcodePractice/isBalanced.java
@@ -0,0 +1,33 @@
+package lettcodePractice;
+/**
+ * Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as a binary tree 
+in which the depth of the two subtrees of every node never differ by more than 1.
+ * 
+ *
+ */
+public class isBalanced {
+	public boolean isBalanced(TreeNode root) {
+        if(root == null) return true;
+        
+        if(Math.abs(height(root.right) - height(root.left)) >= 2){
+        	return false;
+        } else {
+        	return isBalanced(root.right) && isBalanced(root.left);
+        }
+    }
+	
+	public int height(TreeNode n){
+		if(n == null) return 0;
+		
+		return Math.max(height(n.left), height(n.right) ) + 1;
+	}
+	
+	public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	}
+}
