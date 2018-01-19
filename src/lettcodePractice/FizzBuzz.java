50b3545b3dd8c1923db11a4a750c1a5286aa4f34
kokonattuDream <32606269+kokonattuDream@users.noreply.github.com>
2018-01-19 12:16:07 -0400

    Add files via upload

---
+++ src/lettcodePractice/FizzBuzz.java
@@ -0,0 +1,32 @@
+package lettcodePractice;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Write a program that outputs the string representation of numbers from 1 to n.
+
+	But for multiples of three it should output “Fizz” instead of the number 
+	and for the multiples of five output “Buzz”. 
+	
+	For numbers which are multiples of both three and five output “FizzBuzz”.
+ * 
+ *
+ */
+public class FizzBuzz {
+	public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<String>();
+        
+        for(int i = 1; i <= n; i++){
+        	if(i % 15 ==0){
+        		res.add("FizzBuzz");
+        	} else if(i % 5 ==0){
+        		res.add("Buzz");
+        	} else if( i % 3 ==0){
+        		res.add("Fizz");
+        	} else {
+        		res.add(Integer.toString(i));
+        	}
+        }
+        return res;
+    }
+}
